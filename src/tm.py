2612b64c557a436d195fdfe0405290327961184b
alangshur <adlangshur@gmail.com>
2021-06-08 21:08:57 -0700

    Added presale slack bot

--- src/tm.py
+++ src/tm.py
@@ -1,123 +1,156 @@
 from dateutil import parser
 from dateutil.tz import gettz
 from datetime import datetime
-from tabulate import tabulate
+from comms.slack import SlackBot
 import pandas as pd
 import configparser
+import traceback
 import requests
+import time
 
-### INPUTS
+
+### INPUTS ###
 EVENT_CLASSIFICATION = 'music'
 EVENT_COUNTRY_CODE = 'US'
+REFRESH_RATE_SECS = 30 * 60
 
-# get credentials
+### CREDENTIALS ###
 config = configparser.ConfigParser()
 config.read('../config/ticketmaster.ini')
 CONSUMER_KEY = config['credentials']['consumer_key']
 BASE_URL = config['urls']['base_events_url']
 
-# get timezone dt
+### SETUP ###
+slack = SlackBot(test_mode=True)
 tz = gettz('America/New_York')
-dt_now_utc = datetime.utcnow()
-dt_now = dt_now_utc.astimezone(tz)
 
-events_data = []
-page_num = 0
 
-while True:
+def scan_pubic_sales(time_threshold_hours=1):
 
-    query_params = {
-        'apikey': CONSUMER_KEY,
+    # get timezone dt
+    dt_now_utc = datetime.utcnow()
+    dt_now = dt_now_utc.astimezone(tz)
 
-        'size': 200,
-        'page': page_num,
+    events_data = []
+    page_num = 0
 
-        'classificationName': EVENT_CLASSIFICATION,
-        'countryCode': EVENT_COUNTRY_CODE,
+    while True:
 
-        'sort': 'date,asc',
-        'startDateTime': dt_now_utc.strftime('%Y-%m-%dT%H:%M:%SZ'),
-        'onsaleOnAfterStartDate': dt_now_utc.strftime('%Y-%m-%d'),
+        query_params = {
+            'apikey': CONSUMER_KEY,
 
-        'includeTBA': 'no',
-        'includeTBD': 'no',
-        'includeTest': 'no'
-    }
+            'size': 200,
+            'page': page_num,
 
-    response = requests.get(
-        url=BASE_URL,
-        params=query_params
-    )
+            'classificationName': EVENT_CLASSIFICATION,
+            'countryCode': EVENT_COUNTRY_CODE,
 
-    contents = response.json()
-    total_pages = contents['page']['totalPages']
-    events_data.extend(contents['_embedded']['events'])
-    if total_pages == page_num + 1: break
-    else: page_num += 1
+            'sort': 'date,asc',
+            'startDateTime': dt_now_utc.strftime('%Y-%m-%dT%H:%M:%SZ'),
+            'onsaleOnAfterStartDate': dt_now_utc.strftime('%Y-%m-%d'),
 
-public_sale_data = []
-presale_data = []
+            'includeTBA': 'no',
+            'includeTBD': 'no',
+            'includeTest': 'no'
+        }
 
-for event in events_data:
-    try:
-        event_name = event['name']
-        event_dt = parser.parse(event['dates']['start']['dateTime']).astimezone(tz)
-        event_url = event['url']
+        response = requests.get(
+            url=BASE_URL,
+            params=query_params
+        )
 
-        if 'public' in event['sales']:
-            sale = event['sales']['public']
-            sale_name = 'public'
-            sale_start_dt = parser.parse(sale['startDateTime']).astimezone(tz)
-            sale_end_dt = parser.parse(sale['endDateTime']).astimezone(tz)
-            sale_started = bool(dt_now >= sale_start_dt)
-            sale_ended = bool(dt_now >= sale_end_dt)
-            sale_in_progress = bool(sale_started and not sale_ended)
-            if not sale_started: sale_status = 'not_started'
-            elif sale_in_progress: sale_status = 'in_progress'
-            elif sale_ended: sale_status = 'over'
-            public_sale_data.append({
-                'event_name': event_name,
-                'event_url': event_url,
-                'event_dt': event_dt,
-                'sale_starts_in': pd.Timedelta(sale_start_dt - dt_now),
-                'sale_start_dt': sale_start_dt,
-                'sale_end_dt': sale_end_dt,
-                'sale_status': sale_status
-            })
+        contents = response.json()
+        total_pages = contents['page']['totalPages']
+        events_data.extend(contents['_embedded']['events'])
+        if total_pages == page_num + 1: break
+        else: page_num += 1
 
-        if 'presales' in event['sales']:
-            for presale in event['sales']['presales']:
-                sale_name = presale['name']
-                sale_start_dt = parser.parse(presale['startDateTime']).astimezone(tz)
-                sale_end_dt = parser.parse(presale['endDateTime']).astimezone(tz)
-                sale_started = bool(dt_now >= sale_start_dt)
-                sale_ended = bool(dt_now >= sale_end_dt)
+    public_sale_data = []
+    presale_data = []
+
+    for event in events_data:
+        try:
+            event_id = event['id']
+            event_name = event['name']
+            event_dt = parser.parse(event['dates']['start']['dateTime'])
+            event_url = event['url']
+
+            if 'public' in event['sales']:
+                sale = event['sales']['public']
+                sale_name = 'public'
+                sale_start_dt = parser.parse(sale['startDateTime'])
+                sale_end_dt = parser.parse(sale['endDateTime'])
+                sale_started = bool(dt_now_utc >= sale_start_dt)
+                sale_ended = bool(dt_now_utc >= sale_end_dt)
                 sale_in_progress = bool(sale_started and not sale_ended)
                 if not sale_started: sale_status = 'not_started'
                 elif sale_in_progress: sale_status = 'in_progress'
                 elif sale_ended: sale_status = 'over'
-                presale_data.append({
+                public_sale_data.append({
+                    'event_id': event_id,
                     'event_name': event_name,
                     'event_url': event_url,
                     'event_dt': event_dt,
-                    'sale_name': sale_name,
-                    'sale_starts_in': pd.Timedelta(sale_start_dt - dt_now),
+                    'sale_starts_in': pd.Timedelta(sale_start_dt - dt_now_utc),
                     'sale_start_dt': sale_start_dt,
                     'sale_end_dt': sale_end_dt,
                     'sale_status': sale_status
                 })
 
-    except:
-        pass
+            if 'presales' in event['sales']:
+                for presale in event['sales']['presales']:
+                    sale_name = presale['name']
+                    sale_start_dt = parser.parse(presale['startDateTime'])
+                    sale_end_dt = parser.parse(presale['endDateTime'])
+                    sale_started = bool(dt_now_utc >= sale_start_dt)
+                    sale_ended = bool(dt_now_utc >= sale_end_dt)
+                    sale_in_progress = bool(sale_started and not sale_ended)
+                    if not sale_started: sale_status = 'not_started'
+                    elif sale_in_progress: sale_status = 'in_progress'
+                    elif sale_ended: sale_status = 'over'
+                    presale_data.append({
+                        'event_id': event_id,
+                        'event_name': event_name,
+                        'event_url': event_url,
+                        'event_dt': event_dt,
+                        'sale_name': sale_name,
+                        'sale_starts_in': pd.Timedelta(sale_start_dt - dt_now_utc),
+                        'sale_start_dt': sale_start_dt,
+                        'sale_end_dt': sale_end_dt,
+                        'sale_status': sale_status
+                    })
+
+        except Exception as e:
+            print(e)
+
+    if len(public_sale_data) == 0: return None
+    else:
+        public_sale_df = pd.DataFrame.from_records(public_sale_data)
+        public_sale_df = public_sale_df[public_sale_df.sale_status == 'not_started']
+        public_sale_df = public_sale_df[(public_sale_df.sale_starts_in.dt.days) < 1 & (public_sale_df.sale_starts_in.dt.days) < 1]
+        public_sale_df = public_sale_df.sort_values(by='sale_starts_in', ascending=True)
+
+        public_sale_df.index = public_sale_df.event_id
+        public_sale_df = public_sale_df[['event_name', 'sale_starts_in', 'event_url']]
+        return public_sale_df
 
 
-public_sale_df = pd.DataFrame.from_records(public_sale_data)
-public_sale_df = public_sale_df[public_sale_df.sale_status == 'not_started']
-public_sale_df = public_sale_df[public_sale_df.sale_starts_in.dt.days < 1]
-public_sale_df = public_sale_df.sort_values(by='sale_starts_in', ascending=True)
+print('Launching TM sales scanning bot.')
+slack.send_message('tm-sales-scans', '*Info*: Launching TM sales scanning bot.')
 
-public_sale_df.index = public_sale_df.event_name
-public_sale_df = public_sale_df[['sale_starts_in', 'event_url']]
 
-output_df = tabulate(public_sale_df, headers='keys', tablefmt='psql')
-print(output_df, flush=True)
+try:
+    while True:
+        upcoming_sales_df = scan_pubic_sales()
+        print(upcoming_sales_df)
+        time.sleep(REFRESH_RATE_SECS)
+
+except KeyboardInterrupt: 
+    pass
+except Exception as e: 
+    traceback.print_exc()
+    slack.send_message('tm-sales-scans', '*Error*: Internal server error in TM sales scanning bot.')
+
+
+print('\nShutting down TM sales scanning bot.')
+slack.send_message('tm-sales-scans', '*Info*: Shutting down TM sales scanning bot.')
